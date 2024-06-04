e7c71fa934ec4dd80f36c502e1df698bda9a21c5
dtdev <dt@dtdev.de>
2024-06-04 21:45:30 +0200

    Adds Prisma ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from '@app/prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
