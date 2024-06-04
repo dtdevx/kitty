62908d03f9ec6e22c24f9406b96537570b490f9e
dtdev <dt@dtdev.de>
2024-06-04 20:51:30 +0200

    Adds env configuration
    - sets app port via env
    - provides configModule globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
