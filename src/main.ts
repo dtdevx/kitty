282e96936f4557f0b3eb0e7b08aad1a85694bf64
dtdev <dt@dtdev.de>
2024-06-04 23:10:09 +0200

    Adds OpenApi Swagger for development

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Kitty API')
+      .setDescription('This is the Kitty API documentation via Swagger.')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document, {
+      customSiteTitle: 'Kitty API documentation via Swagger',
+    });
+  }
+
   const port = configService.get<number>('APP_PORT', 3000);
 
   await app.listen(port);
